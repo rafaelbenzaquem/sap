c9115a2c79f1596fb8dcc554dc7938f586cc7afa
Rafael <rafael.neto@outlook.com>
2025-04-17 15:59:55 -0400

    Corrigindo bug no recurso de atualização de usuarios

--- src/main/java/br/jus/trf1/sipe/usuario/web/UsuarioUpdateController.java
+++ src/main/java/br/jus/trf1/sipe/usuario/web/UsuarioUpdateController.java
@@ -5,8 +5,13 @@ import br.jus.trf1.sipe.usuario.UsuarioRepository;
 import br.jus.trf1.sipe.usuario.web.dto.UsuarioAtualizadoRequest;
 import br.jus.trf1.sipe.usuario.web.dto.UsuarioResponse;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/v1/sipe/usuarios")
@@ -19,20 +24,27 @@ public class UsuarioUpdateController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UsuarioResponse> atualziarVinculo(@PathVariable Integer id,
-                                                            @RequestBody @Valid UsuarioAtualizadoRequest request) {
+    public ResponseEntity<EntityModel<UsuarioResponse>> atualziarVinculo(@PathVariable Integer id,
+                                                                         @RequestBody
+                                                                         @Valid UsuarioAtualizadoRequest request) {
 
-        var vinculoOpt = repository.findById(id);
+        var usuarioOpt = repository.findById(id);
 
-        if (vinculoOpt.isPresent()) {
-            var vinculo = vinculoOpt.get();
-            vinculo.setNome(request.nome());
-            vinculo.setCracha(request.cracha());
-            vinculo.setMatricula(request.matricula());
+        if (usuarioOpt.isPresent()) {
+            var usuario = usuarioOpt.get();
+            usuario.setNome(request.nome());
+            usuario.setCracha(request.cracha());
+            usuario.setMatricula(request.matricula());
+            usuario.setHoraDiaria(request.horaDiaria());
 
-            var vinculoSalvo = repository.save(vinculo);
+            var usuarioAtualizado = repository.save(usuario);
 
-            return ResponseEntity.ok().body(vinculoSalvo.toResponse());
+            var entityModel = EntityModel.of(
+                    usuarioAtualizado.toResponse(),
+                    linkTo(methodOn(UsuarioReadController.class).buscaVinculo(usuarioAtualizado.getId())).withSelfRel(),
+                    linkTo(methodOn(UsuarioDeleteController.class).apagaVinculo(usuarioAtualizado.getId())).withRel("delete"));
+
+            return ResponseEntity.ok().body(entityModel);
         }
         throw new UsuarioInexistenteException(id);
     }
--- src/main/java/br/jus/trf1/sipe/usuario/web/dto/UsuarioAtualizadoRequest.java
+++ src/main/java/br/jus/trf1/sipe/usuario/web/dto/UsuarioAtualizadoRequest.java
@@ -1,9 +1,17 @@
 package br.jus.trf1.sipe.usuario.web.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record UsuarioAtualizadoRequest(@NotBlank(message = "Campo não pode ser vazio ou em branco.") String nome,
-                                       @NotBlank(message = "Campo não pode ser vazio ou em branco.")String matricula,
-                                       @NotBlank(message = "Campo não pode ser vazio ou em branco.")String cracha) {
+public record UsuarioAtualizadoRequest(@NotBlank(message = "Campo 'nome' não pode ser vazio ou em branco.")
+                                       String nome,
+                                       @NotBlank(message = "Campo 'matricula' não pode ser vazio ou em branco.")
+                                       String matricula,
+                                       @NotBlank(message = "Campo 'cracha' não pode ser vazio ou em branco.")
+                                       String cracha,
+                                       @NotNull(message = "Campo 'hora_diaria' não pode ser ")
+                                       @JsonProperty("hora_diaria")
+                                       Integer horaDiaria) {
 }
