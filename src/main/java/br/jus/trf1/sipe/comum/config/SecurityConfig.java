26b8472b420dd3399cd90c6db0b94cef73b75bcb
Rafael <rafael.neto@outlook.com>
2025-05-29 01:03:55 -0400

    configurando cors para o sipe-web

--- src/main/java/br/jus/trf1/sipe/comum/config/SecurityConfig.java
+++ src/main/java/br/jus/trf1/sipe/comum/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.jus.trf1.sipe.comum.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .authorizeHttpRequests(authz -> authz
                         .anyRequest().authenticated()
