0a1e53b17a6629865734725ddab113dfdc0b8fd7
Rafael <rafael.neto@outlook.com>
2025-06-20 18:48:15 -0400

    Ajuste recursos de folha

--- src/main/java/br/jus/trf1/sipe/folha/web/FolhaController.java
+++ src/main/java/br/jus/trf1/sipe/folha/web/FolhaController.java
@@ -16,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+import static br.jus.trf1.sipe.comum.util.DataTempoUtil.paraString;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -34,22 +38,28 @@ public class FolhaController {
     @PreAuthorize("hasAuthority('GRP_SIPE_USERS')")
     public ResponseEntity<EntityModel<FolhaResponse>> buscaFolha(@RequestParam("matricula")
                                                                  String matricula,
-                                                                 @RequestParam("ano")
-                                                                 Integer ano,
                                                                  @RequestParam("mes")
-                                                                 Integer valorMes) {
+                                                                 Integer valorMes,
+                                                                 @RequestParam("ano")
+                                                                 Integer ano) {
         var mes = Mes.getMes(valorMes);
         log.info("Buscando Folha - {} - {} - {}", matricula, ano, mes.getNome());
 
         var folha = folhaService.buscarFolha(matricula, mes, ano).orElse(folhaService.abrirFolha(matricula, mes, ano));
 
 
-        var uri = ServletUriComponentsBuilder.fromCurrentContextPath().
-                path("/v1/sipe/folhas?matricula={matricula}&ano={ano}&mes={mes}").
-                buildAndExpand(matricula, ano, mes).toUriString();
+        var diaInicial = LocalDate.of(ano, mes.getValor(), 1);
+        var inicio = paraString(diaInicial);
+        var diaFinal = YearMonth.of(ano, mes.getValor()).atEndOfMonth();
+        var fim = paraString(diaFinal);
+
+
+        var uriPonto = ServletUriComponentsBuilder.fromCurrentContextPath().
+                path("/v1/sipe/pontos?matricula={matricula}&inicio={inicio}&fim={fim}").
+                buildAndExpand(matricula, inicio, fim).toUriString();
         var pontoModel = EntityModel.of(FolhaResponse.of(folha),
-                linkTo(methodOn(FolhaController.class).buscaFolha(matricula, ano, valorMes)).withSelfRel(),
-                Link.of(uri).withRel("pontos")
+                linkTo(methodOn(FolhaController.class).buscaFolha(matricula, valorMes, ano)).withSelfRel(),
+                Link.of(uriPonto).withRel("pontos")
         );
         return ResponseEntity.ok(pontoModel);
     }
--- src/main/java/br/jus/trf1/sipe/folha/web/dto/FolhaResponse.java
+++ src/main/java/br/jus/trf1/sipe/folha/web/dto/FolhaResponse.java
@@ -1,18 +1,13 @@
 package br.jus.trf1.sipe.folha.web.dto;
 
 import br.jus.trf1.sipe.folha.Folha;
-import br.jus.trf1.sipe.ponto.web.dto.PontoResponse;
 import lombok.Builder;
-
-import java.util.Collections;
-import java.util.List;
 
 @Builder
 public record FolhaResponse(String matricula,
                             String mes,
                             Integer ano,
-                            Boolean homologado,
-                            List<PontoResponse> pontos) {
+                            Boolean homologado) {
 
     public static FolhaResponse of(Folha folha) {
         return FolhaResponse.builder()
@@ -20,7 +15,6 @@ public record FolhaResponse(String matricula,
                 .mes(folha.getId().getMes().getNome())
                 .ano(folha.getId().getAno())
                 .homologado(folha.getDataHomologacao() != null)
-                .pontos(folha.getPontos() == null ? Collections.emptyList() : folha.getPontos().stream().map(PontoResponse::of).toList())
                 .build();
     }
 }
