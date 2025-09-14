5a63039705d9ba2e84c5b584613ec62b79ab1992
Rafael Benzaquem Neto <rafael.neto@trf1.jus.br>
2025-09-14 09:43:22 -0400

    Implementando utilitário de validação para token jwt proveniente de conexão via websockets

---
+++ src/main/java/br/jus/trf1/sipe/comum/config/JwtTokenUtil.java
@@ -0,0 +1,14 @@
+package br.jus.trf1.sipe.comum.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenUtil {
+    public boolean validateToken(String token) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getUsernameFromToken(String token) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
